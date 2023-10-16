f04879a7ffeec1161361baec8b4994d07799c523
imran-dev100 <iagro7189@gmail.com>
2023-10-16 21:01:46 +0530

    Creating Spring Boot application

---
+++ src/main/java/com/ml/impl/MLImpApplication.java
@@ -0,0 +1,13 @@
+package com.ml.impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MLImpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MLImpApplication.class, args);
+	}
+
+}
